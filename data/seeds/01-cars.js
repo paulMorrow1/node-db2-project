56f0b8347865951ea5284c274686e0cb36a6dba2
Paul Morrow <pmorow93@gmail.com>
2022-06-22 09:33:33 -0500

    migration up function

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,40 @@
 // STRETCH
+const cars = [
+  {
+    vin: "1111111111",
+    make: "toyota",
+    model: "prius",
+    mileage: 21500,
+    title: "clean",
+    transmission: "manual",
+  },
+
+  {
+    vin: "1111111111",
+    make: "toyota",
+    model: "corolla",
+    mileage: 11500,
+    title: "salvage",
+  },
+
+  {
+    vin: "1111111111",
+    make: "ford",
+    model: "focus",
+    mileage: 1500,
+  },
+];
+
+// exports.seed = function (knex) {
+//   return knex("cars")
+//     .truncate()
+//     .then(() => {
+//       return knex("cars").insert(cars);
+//     });
+// };
+
+//async function
+exports.seed = async function (knex) {
+  await knex("cars").truncate();
+  await knex("cars").insert(cars);
+};
